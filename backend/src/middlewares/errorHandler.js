6284bbff594602384bc4a9dc7e7ac49be160abda
Jesus Maldonado <vgtmldnd@gmail.com>
2026-01-29 20:14:36 -0400

    Add: error handler middleware an util

---
+++ backend/src/middlewares/errorHandler.js
@@ -0,0 +1,8 @@
+export const errorHandler = (err, req, res, next) => {
+    const error = {
+        code: err.status || 500,
+        message: err.message || 'Internal server error'
+    }
+
+    res.status(error.status).json({ message: error.message})
+}
