41af491f1ea5484a92cad5e57f3e9bdae90e047e
Jesus Maldonado <vgtmldnd@gmail.com>
2026-02-07 22:13:32 -0400

    Fix: map controller name

--- backend/src/presentation/routes/mapRoutes.js
+++ backend/src/presentation/routes/mapRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as mapController from '../controllers/mapsController.js';
+import * as mapController from '../controllers/mapController.js';
 
 const router = express.Router({ mergeParams: true });
 router
